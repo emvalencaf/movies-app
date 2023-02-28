3d7071384ae2a2e11d888f148b91a5010d35a86f
emvalencaf <edsonmvf@gmail.com>
2023-02-27 23:38:51 -0300

    Update pagination.ts
    fixed pagination

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -6,26 +6,22 @@ export default class PaginationFormat{
 		// first node and last node
 		let nrStart: number = 0;
 		let nrEnd: number = 0;
-		// set navegation
-		if (totalPages < pageLimit) {
-			console.log("totalPages menor que pageLimit");
-			pageLimit = totalPages - currentPage
-			console.log("agora pageLimit é ", pageLimit);
-		};
+
+		if (totalPages < pageLimit) pageLimit = totalPages - currentPage;
 
 		if (currentPage - parseInt((pageLimit / 2).toString()) < 1) {
 			// set first page
 			nrStart = 1;
 			nrEnd = pageLimit;
 
-		} else if (currentPage - parseInt((pageLimit / 2).toString()) > totalPages) {
+		} else if (currentPage - parseInt((pageLimit / 2).toString()) < totalPages) {
 			// set last pages
 			nrStart = totalPages - pageLimit;
 			nrEnd = totalPages;
 		} else {
 			//set middle page
 			nrStart = currentPage - parseInt((pageLimit / 2).toString());
-			totalPages > pageLimit? nrEnd = currentPage + parseInt((pageLimit / 2).toString()) : nrEnd = pageLimit;
+			nrEnd = currentPage + parseInt((pageLimit / 2).toString());
 		}
 
 		return {
